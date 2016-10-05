83f59154b45548459319f23a5593e850768e0cbd
Benjamin George <bmg266@nau.edu>
2016-10-05 00:42:31 -0700

    Implemented Raycasting and PPM (P6) Writing

--- raycast.c
+++ raycast.c
@@ -34,8 +34,8 @@ typedef struct object {
 
 
 typedef struct objectBuffer {
-	object *objectArray;
 	size_t objectCount;
+	object *objectArray;
 } objectBuffer;
 
 
@@ -261,7 +261,7 @@ double* next_vector(FILE* json) {
 }
 
 
-void read_scene(char* filename) {
+struct objectBuffer read_scene(char* filename) {
   int c;
   FILE* json = fopen(filename, "r");
 
@@ -269,6 +269,10 @@ void read_scene(char* filename) {
     fprintf(stderr, "Error: Could not open file \"%s\"\n", filename);
     exit(1);
   }
+
+  objectBuffer objects;
+  objects.objectCount = 0;
+  objects.objectArray = NULL;
   
   skip_ws(json);
   
@@ -284,7 +288,7 @@ void read_scene(char* filename) {
     if (c == ']') {
       fprintf(stderr, "Error: This is the worst scene file EVER.\n");
       fclose(json);
-      return;
+      return objects;
     }
 
     if (c == '{') {
@@ -303,14 +307,21 @@ void read_scene(char* filename) {
 
       char* value = next_string(json);
 
+      // have a new object, so increase the count and realloc the object buffer
+      objects.objectCount++;
+      objects.objectArray = realloc(objects.objectArray, sizeof(object) * objects.objectCount);
+      objects.objectArray[objects.objectCount - 1].type = value;
+
+      // check that type of object is camera, sphere, or plane
+      // if not any of those, throw an error
       if (strcmp(value, "camera") == 0) {
-        fprintf(stdout, "value is: %s\n", value);
+        //fprintf(stdout, "value is: %s\n", value);
         // draw a camera (?)
       } else if (strcmp(value, "sphere") == 0) {
-        fprintf(stdout, "value is: %s\n", value);
+        //fprintf(stdout, "value is: %s\n", value);
         // draw a sphere
       } else if (strcmp(value, "plane") == 0) {
-        fprintf(stdout, "value is: %s\n", value);
+        //fprintf(stdout, "value is: %s\n", value);
         // draw a plane
       } else {
         fprintf(stderr, "Error: Unknown type, \"%s\", on line number %d.\n", value, line);
@@ -341,8 +352,30 @@ void read_scene(char* filename) {
               exit(1);
             }
 
+						if (strcmp(key, "width") == 0) {
+							objects.objectArray[objects.objectCount - 1].camera.width = value;
+						} else if (strcmp(key, "height") == 0) {
+							objects.objectArray[objects.objectCount - 1].camera.height = value;
+						} else if (strcmp(key, "radius") == 0) {
+							objects.objectArray[objects.objectCount - 1].sphere.radius = value;
+						}
+
           } else if ((strcmp(key, "color") == 0) || (strcmp(key, "position") == 0) || (strcmp(key, "normal") == 0)) {
             double* value = next_vector(json);
+
+            if (strcmp(key, "color") == 0) {
+            	objects.objectArray[objects.objectCount - 1].color[0] = value[0];
+            	objects.objectArray[objects.objectCount - 1].color[1] = value[1];
+            	objects.objectArray[objects.objectCount - 1].color[2] = value[2];
+            } else if (strcmp(key, "position") == 0) {
+            	objects.objectArray[objects.objectCount - 1].sphere.position[0] = value[0];
+            	objects.objectArray[objects.objectCount - 1].sphere.position[1] = value[1];
+            	objects.objectArray[objects.objectCount - 1].sphere.position[2] = value[2];
+            } else if (strcmp(key, "normal") == 0) {
+            	objects.objectArray[objects.objectCount - 1].plane.normal[0] = value[0];
+            	objects.objectArray[objects.objectCount - 1].plane.normal[1] = value[1];
+            	objects.objectArray[objects.objectCount - 1].plane.normal[2] = value[2];
+            }
 
           } else {
             fprintf(stderr, "Error: Unknown property, \"%s\", on line %d.\n", key, line);
@@ -365,7 +398,7 @@ void read_scene(char* filename) {
         skip_ws(json);
       } else if (c == ']') {
         fclose(json);
-        return;
+        return objects;
       } else {
         fprintf(stderr, "Error: Expecting ',' or ']' on line %d.\n", line);
         exit(1);
@@ -375,15 +408,179 @@ void read_scene(char* filename) {
 }
 
 
+double sqr(double v) {
+  return v*v;
+}
+
+
+void normalize(double* v) {
+  double len = sqrt(sqr(v[0]) + sqr(v[1]) + sqr(v[2]));
+  v[0] /= len;
+  v[1] /= len;
+  v[2] /= len;
+}
+
+
+void color_pixel(double *color, int row, int col, imageData *data) {
+	data->color[(int)((row * data->width * 3) + (col * 3))] = (char)(color[0] * 255);
+	data->color[(int)((row * data->width * 3) + (col * 3 + 1))] = (char)(color[1] * 255);
+	data->color[(int)((row * data->width * 3) + (col * 3 + 2))] = (char)(color[2] * 255);
+}
+
+
+double plane_intersection(double *Ro, double *Rd, double *position, double *normal) {
+	double a = (normal[0] * Rd[0]) + (normal[1] * Rd[1]) + (normal[2] * Rd[2]);
+
+	if (fabs(a) < 0.0001) {
+		return -1;
+	}
+
+	double b[3];
+
+	for (int i = 0; i <= 2; i++) {
+		b[i] = position[i] - Ro[i];
+	}
+
+	double d = (b[0] * normal[0]) + (b[1] * normal[1]) + (b[2] * normal[2]);
+
+	double t = d/a;
+
+	if (t < 0) {
+		return -1;
+	}
+
+	return t;
+}
+
+
+double sphere_intersection(double *Ro, double *Rd, double *C, double r) {
+  double a = (sqr(Rd[0]) + sqr(Rd[1]) + sqr(Rd[2]));
+  double b = 2 * (Rd[0] * (Ro[0] - C[0]) + Rd[1] * (Ro[1] - C[1]) + Rd[2] * (Ro[2] - C[2]));
+  double c = sqr(Ro[0] - C[0]) + sqr(Ro[1] - C[1]) + sqr(Ro[2] - C[2]) - sqr(r);
+
+  double det = sqr(b) - 4 * a * c;
+  if (det < 0) return -1;
+
+  det = sqrt(det);
+
+  double t0 = (-b - det) / (2*a);
+  if (t0 > 0) return t0;
+
+  double t1 = (-b + det) / (2*a);
+  if (t1 > 0) return t1;
+
+  return -1;
+}
+
+
+void writeP6(imageData *image, FILE* output, int magicNumber) {
+
+  size_t num;
+  int size = image->width * image->height * 4;
+  if (magicNumber == 6) {
+    fprintf(output, "P%d\n%lf %lf\n%d\n", magicNumber, image->width, image->height, 255);
+    for (int i = 0; i < size; i++) {
+      char c = image->color[i];
+      if (i%4 != 0) {
+        fwrite(&c, 1, 1, output);
+      }
+    }
+  }
+}
+
+
 int main(int argc, char *argv[]) {
 	check_arguments(argc, argv);
 
-	//FILE *json = fopen(argv[3], "r");
+	char *json = argv[3];
 
-	char *filename = argv[3];
+	objectBuffer objects;
+	int objectCount;
 
-	read_scene(filename);
+	int cameraNum = 0;
+	double cameraHeight;
+	double cameraWidth;
 
-	fprintf(stdout, "Hey hey! The program didn't crash!\n");
+	int sphereNum = 1;
+	double sphereColorR, sphereColorG, sphereColorB;
+	double spherePositionX, spherePositionY, spherePositionZ;
+	double sphereRadius;
+
+	int planeNum = 2;
+	double planeColorR, planeColorG, planeColorB;
+	double planePositionX, planePositionY, planePositionZ;
+	double planeNormalX, planeNormalY, planeNormalZ;
+
+	int M = atoi(argv[1]);
+	int N = atoi(argv[2]);
+
+	objects = read_scene(json);
+
+	objectCount = objects.objectCount;
+
+	int i;
+	int magicNumber = 6;
+
+	for (i = 0; i < objectCount; i++) {
+		if (objects.objectArray[i].camera.width && objects.objectArray[i].camera.height) {
+			cameraWidth = objects.objectArray[i].camera.width;
+			cameraHeight = objects.objectArray[i].camera.height;
+		}
+	}
+
+	if (!cameraWidth || !cameraHeight) {
+		fprintf(stderr, "Error: One or both camera dimensions are invalid.\n");
+		exit(1);
+	}
+
+	double pixelWidth = cameraWidth / M;
+	double pixelHeight = cameraHeight / N;
+
+	imageData *image = (imageData *)malloc(sizeof(imageData));
+  image->height = N;
+  image->width = M;
+  image->color = (char*)malloc(sizeof(char) * image->height * image->width * 4);
+
+
+  // loop through the pixels
+  for (int y = 0; y < N; y++) {
+    for (int x = 0; x < M; x++) {
+      double Ro[3] = {0, 0, 0};
+
+      double Rd[3] = {objects.objectArray[i].sphere.position[0] - (cameraWidth/2) + pixelWidth * (x + 0.5),
+      								objects.objectArray[i].sphere.position[1] - (cameraHeight/2) + pixelHeight * (y + 0.5), 1 };
+      normalize(Rd);
+
+      int colorHelper = 0;
+      double best_t = INFINITY;
+      // loop through the objects figuring out which one to trace
+      for (i = 0; i < objectCount; i++) {
+        double t = 0;
+
+        if (objects.objectArray[i].sphere.position && objects.objectArray[i].sphere.radius) {
+          t = sphere_intersection(Ro, Rd, objects.objectArray[i].sphere.position, objects.objectArray[i].sphere.radius);// if the current object is a plane then call intersect
+        } else if (objects.objectArray[i].plane.position && objects.objectArray[i].plane.normal) {
+          t = plane_intersection(Ro, Rd, objects.objectArray[i].plane.position, objects.objectArray[i].plane.normal);
+        }
+        
+        if (t > 0 && t < best_t) {
+          best_t = t;
+          colorHelper = i;
+        }
+      }
+      if (best_t > 0 && best_t != INFINITY) {
+        if (objects.objectArray[colorHelper].sphere.position && objects.objectArray[colorHelper].sphere.radius) {
+          color_pixel(objects.objectArray[colorHelper].color, y, x, image);
+        } else if (objects.objectArray[i].plane.position && objects.objectArray[i].plane.normal) {
+          color_pixel(objects.objectArray[colorHelper].color, y, x, image);
+        }
+      }
+    }
+  }
+
+  FILE* output = fopen(argv[4], "w+");
+  writeP6(image, output, magicNumber);
+  fclose(output);
+
 	return(0);
 }
